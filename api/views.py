1a254f74e886557938b7eb45833935510353239d
mario <codemario318@gmail.com>
2024-03-04 16:22:21 +0900

    [ADD] BlogPost 전체 조회 및 생성 추가

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
