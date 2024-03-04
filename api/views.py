3226f847a5086db2d6b38b5fecdf0ab068a19f34
mario <codemario318@gmail.com>
2024-03-04 16:26:47 +0900

    [ADD] BlogPost 커스텀뷰 추가

--- api/views.py
+++ api/views.py
@@ -19,3 +19,17 @@ class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookup_field = "pk"
+
+
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get("title", "")
+
+        if title:
+            blog_posts = BlogPost.objects.filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+
+        serializer = BlogPostSerializer(blog_posts, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
